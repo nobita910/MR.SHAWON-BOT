592853eb832bcb1f2daf6bded20469c9dabb606f
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-18 19:39:28 +0600

    Update commands.js

--- scripts/commands/commands.js
+++ scripts/commands/commands.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "command",
     version: "1.0.0",
     permission: 2,
-    credits: "nayan",
+    credits: "SHAWON",
     description: "manage/control all bot modules",
     prefix: true,
     category: "operator",
