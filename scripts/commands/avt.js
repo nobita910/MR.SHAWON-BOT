e58d7aaf41af67151816b17d2292c0e4a54ade36
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-18 19:33:06 +0600

    Update avt.js

--- scripts/commands/avt.js
+++ scripts/commands/avt.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "avt",
   version: "1.0.0",
   permission: 0,
-  credits: "Nayan",
+  credits: "SHAWON",
   description: "Avt pic",
   prefix: true, 
   category: "user", 
