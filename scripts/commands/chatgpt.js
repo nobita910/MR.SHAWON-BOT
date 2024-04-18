bbb40034e918f4e6760facfc1ae98485283da07a
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-18 19:39:01 +0600

    Update chatgpt.js

--- scripts/commands/chatgpt.js
+++ scripts/commands/chatgpt.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-  name: "nayan",
+  name: "shawon",
 	version: "1.0.0",
 	permission: 0,
   prefix: false,
-	credits: "Nayan",
-	description: "I am nayan better than ChatGPT and Bard",
+	credits: "Shawon",
+	description: "I am shawon better than ChatGPT and Bard",
 	category: "user",
 	cooldowns: 5
 };
