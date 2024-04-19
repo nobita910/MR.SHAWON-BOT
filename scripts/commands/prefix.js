81d404b4402e859ef7d86d5a36ee62e70cf1418d
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-19 10:19:38 +0600

    Update prefix.js

--- scripts/commands/prefix.js
+++ scripts/commands/prefix.js
@@ -1,17 +1,18 @@
 module.exports.config = {
   name: "prefix",
-  version: "1.0.0",
-  permission: 0,
-  credits: "ryuko",
-  prefix: true,
-  description: "guide",
-  category: "system",
-  usages: "",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Mr Chand",
+  description: "See the bot prefix",
+  commandCategory: "For admin",
+  usages: "out prefix",
   cooldowns: 5,
 };
 
 module.exports.handleEvent = async ({ event, api, Threads }) => {
   var { threadID, messageID, body, senderID } = event;
+  //if (senderID == global.data.botID) return;
+  if ((this.config.credits) != "Mr Chand") { return api.sendMessage(`Changed credits!`, threadID, messageID)}
   function out(data) {
     api.sendMessage(data, threadID, messageID)
   }
@@ -19,20 +20,20 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
   var data = dataThread.data; 
   const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
 
-  var arr = ["mpre","mprefix","prefix", "command mark", "What is the prefix of the bot?","PREFIX"];
+  var arr = ["mpre","mprefix","prefix", "dấu lệnh", "prefix của bot là gì","daulenh", "duong"];
   arr.forEach(i => {
     let str = i[0].toUpperCase() + i.slice(1);
     if (body === i.toUpperCase() | body === i | str === body) {
-		const prefix = threadSetting.PREFIX || global.config.PREFIX;
-      if (config.PREFIX == null) {
-        return out(`bot prefix : ${global.config.PREFIX}`)
+const prefix = threadSetting.PREFIX || global.config.PREFIX;
+      if (data.PREFIX == null) {
+        return out(`️️️️️️️️️️️️️️️️️️️️️️️️️🚀NOBITA-ROBOT PREFIX ⇉ [ ${prefix} ]`)
       }
-      else return out(`bot prefix : ${global.config.PREFIX}`)
+      else return out(`️️️️️️️️️️️️️️️️️️️️️️️️️🛸NOBITA-ROBOT PREFIX  ⇉ 👉🏻 [ ${prefix} ]  `    + data.PREFIX )
     }
 
   });
 };
 
 module.exports.run = async({ event, api }) => {
-    return api.sendMessage("no prefix commands", event.threadID)
-}
+    return api.sendMessage(`️️️️️️️️️️️️️️️️️️️️️️️️️This is my prefix⇉ [ ${global.config.PREFIX} ]`, event.threadID)
+      }
