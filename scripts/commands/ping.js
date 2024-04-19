41be47835ae607291aec52ad8620e224240be2f2
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-19 10:10:25 +0600

    Update ping.js

--- scripts/commands/ping.js
+++ scripts/commands/ping.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "ping",
 	version: "1.0.4",
 	permssion: 0,
-	credits: "NAYAN",
+	credits: "ShAWON",
 	description: "Tag all members",
   prefix: true,
 	category: "Group",
