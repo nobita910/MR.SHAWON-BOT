83c78feb1927dbd23ab538b480c1d8972df23a62
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-19 10:28:37 +0600

    Update setprefix.js

--- scripts/commands/setprefix.js
+++ scripts/commands/setprefix.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "0.0.2",
 	permission: 2,
   prefix: false,
-	credits: "nayan",
+	credits: "Shawon",
 	description: "Change prefix",
 	category: "admin",
 	usages: "prefix",
