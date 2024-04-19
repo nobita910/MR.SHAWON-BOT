e7bf33f15cc09bd21f832b0309ae0b6d4b13a44d
NOBITA-CHOWDHURY <162557199+nobita910@users.noreply.github.com>
2024-04-19 10:26:20 +0600

    Update sendmsg.js

--- scripts/commands/sendmsg.js
+++ scripts/commands/sendmsg.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "0.0.2",
 	permission: 2,
   prefix: true,
-	credits: "Nayan",
+	credits: "Shawon",
 	description: "sendmsg",
 	category: "admin",
 	usages: "sendmsg [user]/[thread] id msg",
